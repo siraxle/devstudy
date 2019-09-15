22237c612acdb25623a894aec6c7d506f26eee14
Evgeney <sir.axle@yandex.ru>
2019-09-15 21:40:29 +0300

    lesson_1

---
+++ src/net/devstudy/jse/lection01_classes_objects/DefaultTest.java
@@ -0,0 +1,12 @@
+package net.devstudy.jse.lection01_classes_objects;
+
+public class DefaultTest {
+    Student student;
+    String parameterName;
+    int intValue;
+    double doubleValue;
+    boolean booleanValue;
+    int[] intArray;
+    Student[] studentArray;
+    boolean[] booleanArray;
+}
