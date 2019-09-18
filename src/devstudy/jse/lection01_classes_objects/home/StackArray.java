5fdfcbdc463b1c8ce8512aac3f1c012398454563
Evgeney <sir.axle@yandex.ru>
2019-09-19 00:02:19 +0300

    add StackArray

---
+++ src/devstudy/jse/lection01_classes_objects/home/StackArray.java
@@ -0,0 +1,56 @@
+package devstudy.jse.lection01_classes_objects.home;
+
+public class StackArray {
+
+    private int size;
+    private int[] array;
+    private int top;
+
+    public StackArray(int s){
+        size = s;
+        array = new int[size];
+        top = -1;
+    }
+
+    public boolean isEmpty(){
+        return (top == -1);
+    }
+
+    public void push(int a){
+        int i = ++top;
+        array[i] = a;
+        System.out.println("Top is " + top);
+    }
+
+    public int pop(){
+        if (isEmpty()){
+            System.out.println("Stack is empty");
+            return 0;
+        }else {
+            System.out.println("Top is " + top);
+            return array[top--];
+        }
+    }
+
+    public int pick(){
+        if (isEmpty()){
+            System.out.println("Stack is empty");
+            return 0;
+        }else {
+            System.out.println("Top is " + top);
+            return array[top];
+        }
+    }
+
+
+    public static void main(String[] args) {
+        StackArray stack = new StackArray(5);
+        System.out.println(stack.top);
+        stack.push(5);
+        stack.push(2);
+        stack.push(1);
+        System.out.println(stack.pick());
+    }
+
+
+}
