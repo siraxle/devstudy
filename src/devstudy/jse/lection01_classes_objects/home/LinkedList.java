34057ed6bb797217726ee2890ada9f7c8f5c8cc2
Evgeney <sir.axle@yandex.ru>
2019-09-21 23:19:35 +0300

    add remove and toArray in LinkedList class

--- src/devstudy/jse/lection01_classes_objects/home/LinkedList.java
+++ src/devstudy/jse/lection01_classes_objects/home/LinkedList.java
@@ -1,5 +1,8 @@
 package devstudy.jse.lection01_classes_objects.home;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class LinkedList {
 
     private Item first;
@@ -34,7 +37,41 @@ public class LinkedList {
         return temp;
     }
     public int remove(int index) {
-
+        //remove first element
+        if (index == 0){
+            Item temp = first;
+            first = temp.getNext();
+            first.setNext(null);
+            size--;
+            System.out.println(temp.getValue());
+            return temp.getValue();
+        } else if (index <= size){
+            if (index == size-1){
+                Item temp = last;
+                last.getPrevious().setNext(null);
+                last.setPrevious(null);
+                size--;
+                System.out.println(temp.getValue());
+                return temp.getValue();
+            }
+            Item temp = first;
+            int i = 0;
+            int res = 0;
+            while (i <= index){
+                if (i == index){
+                    temp.getPrevious().setNext(temp.getNext());
+                    temp.getNext().setPrevious(temp.getPrevious());
+                    res = temp.getValue();
+                    temp.setPrevious(null);
+                    temp.setNext(null);
+                }
+                temp = temp.getNext();
+                i++;
+            }
+            size--;
+            System.out.println(res);
+            return res;
+        }
         return 0;
     }
     public int size(){
@@ -44,8 +81,17 @@ public class LinkedList {
         size = 0;
         first = last = null;
     }
-    public int[] toArray(){
-        return null;
+    public int[] toArray(LinkedList linkedList){
+        Item item = linkedList.first;
+        int[] array = new int[10];
+        int i = 0;
+        while (i <= size - 1){
+            array[i] = item.getValue();
+            item = item.getNext();
+            i++;
+        }
+        System.out.println(Arrays.toString(array));
+        return array;
     }
 
 }
--- src/devstudy/jse/lection01_classes_objects/home/LinkedListTest.java
+++ src/devstudy/jse/lection01_classes_objects/home/LinkedListTest.java
@@ -2,10 +2,16 @@ package devstudy.jse.lection01_classes_objects.home;
 
 public class LinkedListTest {
     public static void main(String[] args) {
-        LinkedList arr = new LinkedList();
+        LinkedList linkedList = new LinkedList();
         for(int i=0;i<10;i++){
-            arr.add(i);
+            linkedList.add(i);
         }
-        arr.get(3);
+        linkedList.toArray(linkedList);
+        //arr.get(3);
+        linkedList.remove(3);
+        linkedList.toArray(linkedList);
+        linkedList.remove(0);
+        linkedList.toArray(linkedList);
+
     }
 }
